4d2a8e91e0fee137ecb3f77632093acf2ec27b50
LadyEmberReverie <decemberredinger@gmail.com>
2022-01-23 14:45:33 -0800

    test

--- BouncyBall.py
+++ BouncyBall.py
@@ -6,3 +6,8 @@
 
 # Project Start: 1/23/2022
 # Class: CS 320
+
+# Feature: Ball will be pointed in a direction with a specific force and simulate the bounces.
+# Feature: Ball will be able to change colors to suit the mood of the user.
+
+from graphics import *
