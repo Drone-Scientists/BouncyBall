624ff68529b94647c6955fd8b5df69541c59c8f5
LadyEmberReverie <decemberredinger@gmail.com>
2022-01-25 13:50:05 -0800

    First major setup, adds a window with a circle on it.

--- BouncyBall.py
+++ BouncyBall.py
@@ -11,3 +11,23 @@
 # Feature: Ball will be able to change colors to suit the mood of the user.
 
 from graphics import *
+
+
+# Maybe this problem can work in graphics. I'd have to figure out how to draw something and then re-draw it.
+# First, let's get our window open.
+
+def main():
+
+    # pt is a placeholder point that will be replaced with a mousehandler
+    pt = Point(100, 100)
+
+    # Ball color is a variable that will be changeable later
+    ballColor = "red"
+    win = GraphWin("BouncyBall.py", 500, 500)
+    ball = Circle(pt, 10)
+    ball.setFill(ballColor)
+    ball.draw(win)
+    win.getKey()
+
+
+main()
