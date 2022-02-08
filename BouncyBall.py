7f8ff5468bd94b8819471c1397c99a7c8884ffcf
LadyEmberReverie <decemberredinger@gmail.com>
2022-02-08 11:17:29 -0800

    First version of Ball Class, plus some junk files.

--- BouncyBall.py
+++ BouncyBall.py
@@ -12,9 +12,50 @@
 
 from graphics import *
 
-
 # Maybe this problem can work in graphics. I'd have to figure out how to draw something and then re-draw it.
 # First, let's get our window open.
+
+
+class ballObject:
+
+    def __init__(self):
+        self.radius = 10
+        self.velocity = 0
+        self.acceleration = 0
+        self.coordinates = Point(100, 100)
+
+    # These functions set different variables for the ball
+
+    # Radius function determines how large the ball is.
+    def setRadius(self, radius):
+        self.radius = radius
+
+    # Velocity function determines how fast the ball is moving.
+    def setVelocity(self, velocity):
+        self.velocity = velocity
+
+    # Acceleration function determines how fast the velocity of the ball is changing.
+    def setAcceleration(self, acceleration):
+        self.acceleration = acceleration
+
+    # Coordinates function determines where the ball is on the coordinates
+    def setCoordinates(self, coordinates):  # coordinates is a Point data type
+        self.coordinates = coordinates
+
+    # These functions get different values for the balls attributes
+
+    def getRadius(self):
+        return self.radius
+
+    def getVelocity(self):
+        return self.velocity
+
+    def getAcceleration(self):
+        return self.acceleration
+
+    def getCoordinates(self):
+        return self.coordinates
+
 
 def main():
 
