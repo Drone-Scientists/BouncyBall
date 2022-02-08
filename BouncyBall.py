83cd95ded38e1838d793261f88eb7837edbe802f
LadyEmberReverie <decemberredinger@gmail.com>
2022-02-08 13:32:23 -0800

    Wall class and working Ball into main

--- BouncyBall.py
+++ BouncyBall.py
@@ -22,7 +22,8 @@ class ballObject:
         self.radius = 10
         self.velocity = 0
         self.acceleration = 0
-        self.coordinates = Point(100, 100)
+        self.coordinatesx = 100
+        self.coordinatesy = 100
 
     # These functions set different variables for the ball
 
@@ -39,8 +40,10 @@ class ballObject:
         self.acceleration = acceleration
 
     # Coordinates function determines where the ball is on the coordinates
-    def setCoordinates(self, coordinates):  # coordinates is a Point data type
-        self.coordinates = coordinates
+    # coordinates is a Point data type
+    def setCoordinates(self, coordinatesx, coordinatesy):
+        self.coordinatesx = coordinatesx
+        self.coordinatesy = coordinatesy
 
     # These functions get different values for the balls attributes
 
@@ -53,21 +56,55 @@ class ballObject:
     def getAcceleration(self):
         return self.acceleration
 
-    def getCoordinates(self):
-        return self.coordinates
+    def getCoordinatesx(self):
+        return self.coordinatesx
+
+    def getCoordinatesy(self):
+        return self.coordinatesy
+
+
+class wallObject:
+
+    def __init__(self):
+
+        self.length = 50
+        self.width = 10
+        self.coordinatesx = 50
+        self.coordinatesy = 50
+
+    def setLength(self, length):
+        self.length = length
+
+    def setWidth(self, width):
+        self.width = width
+
+    def setCoordinates(self, coordinatesx, coordinatesy):
+        self.coordinatesx = coordinatesx
+        self.coordinatesy = coordinatesy
+
+    def getLength(self):
+        return self.length
+
+    def getWidth(self):
+        return self.width
+
+    def getCoordinatesx(self):
+        return self.coordinatesx
+
+    def getCoordinatesy(self):
+        return self.coordinatesy
 
 
 def main():
 
-    # pt is a placeholder point that will be replaced with a mousehandler
-    pt = Point(100, 100)
+    ball = ballObject()
 
     # Ball color is a variable that will be changeable later
     ballColor = "red"
     win = GraphWin("BouncyBall.py", 500, 500)
-    ball = Circle(pt, 10)
-    ball.setFill(ballColor)
-    ball.draw(win)
+    dot = Circle(Point(ball.coordinatesx, ball.coordinatesy), ball.radius)
+    dot.setFill(ballColor)
+    dot.draw(win)
     win.getKey()
 
 
