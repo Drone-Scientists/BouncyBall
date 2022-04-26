edc1f2f3dd29d578e7153aafa81e24de443ddd8a
LadyEmberReverie <decemberredinger@gmail.com>
2022-04-26 11:56:09 -0700

    BouncyBall3_TheRefactoring.py Version 1.0 !!!
    Very exciting.

--- BouncyBall3_TheRefactoring.py
+++ BouncyBall3_TheRefactoring.py
@@ -12,17 +12,19 @@ class ballInformation():
     def __init__(self):
 
         # speed information
+
+        self.positionX = 375
+        self.positionY = 500
         self.speedX = 0
-        self.speedY = 10
+        self.speedY = -10
+        self.collisionX = self.positionX
+        self.collisionY = self.positionY
         self.velocityX = 0
         self.velocityY = 0
         self.acceleration = -9.8
 
         self.radius = 10
         self.color = "blue"
-
-        self.positionX = 375
-        self.positionY = 500
 
 
 def angleFinder(velocityX, velocityY):
@@ -31,19 +33,21 @@ def angleFinder(velocityX, velocityY):
     return angle
 
 
-def deltaDistance(speed, time):
+def deltaDistance(speed):
 
     # This function uses the speed unit vector to determine how far the ball travels in a direction.
 
-    deltaD = speed * time
+    deltaD = 0
+    deltaD = deltaD + speed
     return deltaD
 
 
-def deltaSpeed(velocity, time):
+def deltaSpeed(velocity):
 
     # This function uses velocity, acceleration, and time to figure out the change in speed
 
-    deltaS = velocity * time
+    deltaS = 0
+    deltaS = deltaS + velocity
     return deltaS
 
 
@@ -69,7 +73,7 @@ def velocityLoss(velocity):
 
     # This function returns a velocity that is 25 percent less than the input velocity.
 
-    velocity = velocity * -.75
+    velocity = velocity * -.25
     return velocity
 
 # These next 6 functions adjust the ball graphic object to where it needs to go.
@@ -142,47 +146,55 @@ def collisionDetector(ballInformation, window, ballGraphic):
     print("Window dimensions | LargerEndX : ",
           largerEndX, " largerEndY: ", largerEndY)
 
-    if ballInformation.positionY < ballInformation.radius:
+    if ballInformation.collisionY < ballInformation.radius:
 
         # Set the ball's position in the information and the graphic to 1 ball distance away from the edge of window
 
         ballInformation.positionY = ballInformation.radius
         afterCollisionAdjusterY(ballInformation, ballGraphic)
+        ballInformation.collisionY = ballInformation.positionY
         ballCollision = True
 
-    if ballInformation.positionY > largerEndY:
+    if ballInformation.collisionY > largerEndY:
 
         ballInformation.positionY = largerEndY
         afterCollisionAdjusterYLargerEnd(
             ballInformation, ballGraphic, largerEndY)
+        ballInformation.collisionY = ballInformation.positionY
         ballCollision = True
 
-    if ballInformation.positionX < ballInformation.radius:
+    if ballInformation.collisionX < ballInformation.radius:
 
         ballInformation.positionX = ballInformation.radius
         afterCollisionAdjusterX(ballInformation, ballGraphic)
+        ballInformation.collisionX = ballInformation.positionX
         ballCollision = True
 
-    if ballInformation.positionX > largerEndX:
+    if ballInformation.collisionX > largerEndX:
 
         ballInformation.positionX = largerEndX
         afterCollisionAdjusterXLargerEnd(
             ballInformation, ballGraphic, largerEndX)
+        ballInformation.collisionX = ballInformation.positionX
         ballCollision = True
 
-    if ballInformation.positionX < ballInformation.radius and ballInformation.positionY < ballInformation.radius:
+    if ballInformation.collisionX < ballInformation.radius and ballInformation.collisionY < ballInformation.radius:
 
         ballInformation.positionX = ballInformation.radius
         ballInformation.positionY = ballInformation.radius
         afterCollisionAdjusterCorner(ballInformation, ballGraphic)
+        ballInformation.collisionX = ballInformation.positionX
+        ballInformation.collisionY = ballInformation.positionY
         ballCollision = True
 
-    if ballInformation.positionX > largerEndX and ballInformation.positionY > largerEndY:
+    if ballInformation.collisionX > largerEndX and ballInformation.collisionY > largerEndY:
 
         ballInformation.positionX = largerEndX
         ballInformation.positionY = largerEndY
         afterCollisionAdjusterCornerLargerEnd(
             ballInformation, ballGraphic, largerEndX, largerEndY)
+        ballInformation.collisionX = ballInformation.positionX
+        ballInformation.collisionY = ballInformation.positionY
         ballCollision = True
 
     print("Collision = ", ballCollision)
@@ -204,8 +216,16 @@ def updateBallState(ballInformation, window, ballGraphic, time):
 
     # Use the current ball information to figure out the change in X and/or Y values
 
-    ballXD = deltaDistance(ballInformation.velocityX, time)
-    ballYD = deltaDistance(ballInformation.velocityY, time)
+    BBXD = deltaDistance(ballInformation.speedX)
+    BBYD = deltaDistance(ballInformation.speedY)
+    PX = ballInformation.positionX
+    PY = ballInformation.positionY
+    ballInformation.collisionX = PX + BBXD
+    ballInformation.collisionY = PY + BBYD
+    collideX = ballInformation.collisionX
+    collideY = ballInformation.collisionY
+
+    print("collisionX = ", collideX, " | collisionY = ", collideY)
 
     # check for collisions and deal with them.
 
@@ -215,12 +235,16 @@ def updateBallState(ballInformation, window, ballGraphic, time):
     if collision == True:
 
         collisionDealer(ballInformation)
-        ballXD = deltaDistance(ballInformation.velocityX, time)
-        ballYD = deltaDistance(ballInformation.velocityY, time)
+        BBSX = ballInformation.speedX
+        BBSY = ballInformation.speedY
+        ballInformation.speedX = BBSX * -1
+        ballInformation.speedY = BBSY * -1
+        BBXD = deltaDistance(ballInformation.speedX)
+        BBYD = deltaDistance(ballInformation.speedY)
 
-    print("The balls Y delta is: ", ballYD)
-    ballInformation.positionX = ballInformation.positionX + ballXD
-    ballInformation.positionY = ballInformation.positionY + ballYD
+    print("The balls Y delta is: ", BBYD)
+    ballInformation.positionX = ballInformation.positionX + BBXD
+    ballInformation.positionY = ballInformation.positionY + BBYD
 
     # move the ball in ballGraphic to match the new ballInformation
     # Moving the ball here is fairly complicated but basically you subtract the graphics coordinates from
@@ -230,27 +254,47 @@ def updateBallState(ballInformation, window, ballGraphic, time):
     ballX = ballPosition.getX()
     ballY = ballPosition.getY()
 
-    ballXD = ballInformation.positionX - ballX
-    ballYD = ballInformation.positionY - ballY
+    BBXD2 = ballInformation.positionX - ballX
+    BBYD2 = ballInformation.positionY - ballY
 
-    ballGraphic.move(ballXD, ballYD)
+    ballGraphic.move(BBXD2, BBYD2)
     print("ball moved! : ", time)
-    print("X Delta : ", ballXD, " | Y Delta : ", ballYD)
+    print("X Delta : ", BBXD2, " | Y Delta : ", BBYD2)
 
+    speedX = ballInformation.speedX
+    speedY = ballInformation.speedY
     velocityX = ballInformation.velocityX
     velocityY = ballInformation.velocityY
     positionX = ballInformation.positionX
     positionY = ballInformation.positionY
 
-    # use the deltaVelocity functin to find the new velocity and set it
+    # Change the speed
+    ballInformation.speedX = deltaSpeed(ballInformation.velocityX)
+    ballInformation.speedY = deltaSpeed(ballInformation.velocityY)
 
+    # Change the velocity
     ballInformation.velocityY = deltaVelocityY(
         velocityY, ballInformation.acceleration)
     ballInformation.velocityX = deltaVelocityX(velocityX)
 
+    print("Speed X: ", speedX)
+    print("Speed Y: ", speedY)
     print("Velocity X: ", velocityX)
     print("Velocity Y: ", velocityY)
     print("Position X: ", positionX, " Position Y: ", positionY)
+
+
+def BBSim(ballInformation, ballGraphic, window, timeMax):
+
+    t = 0
+    while t < timeMax:
+
+        updateBallState(ballInformation, window, ballGraphic, t)
+        time.sleep(.5)
+        t = t + 1
+        print(t)
+
+    keyPress = window.getKey()
 
 
 def main():
@@ -266,18 +310,9 @@ def main():
     ball.setFill("blue")
     ball.draw(wn)
 
+    t = 50
     print("Initial Position: ", BB.positionX, " , ", BB.positionY)
-    # initialize time
-    t = 0
-
-    while t < 10:
-
-        updateBallState(BB, wn, ball, t)
-        time.sleep(1)
-        t = t + 1
-        print(t)
-
-    keyPress = wn.getKey()
+    BBSim(BB, ball, wn, t)
 
 
 main()
